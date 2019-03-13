9399da42feac8ccdbd81ded0cab0cfc72a8c5e07
Arthur Rocha <arthur9869@gmail.com>
2019-03-13 15:41:17 -0300

    First MVC - Department.

---
+++ WebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>() {
+                new Department(1, "Electronics"),
+                new Department(2, "Fashion"),
+            };
+            return View(departments);
+        }
+    }
+}
