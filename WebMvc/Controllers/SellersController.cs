fcfd639c6dbcc6e9381d41b0783c074920a496b3
Arthur Rocha <arthur9869@gmail.com>
2019-03-14 15:50:24 -0300

    Sellers controller.

---
+++ WebMvc/Controllers/SellersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly WebMvcContext _context;
+
+        public SellersController(WebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
